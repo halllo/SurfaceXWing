7a80ddd6917136c683e0755b56a2544ba8d4eba0
Manuel Naujoks <mnaujoks@gmail.com>
2016-08-14 12:22:37 +0200

    Squadrons are remembered, more warnings and error messages

--- SurfaceXWing.CompanionApp/SurfaceXWing.CompanionApp/App.xaml.cs
+++ SurfaceXWing.CompanionApp/SurfaceXWing.CompanionApp/App.xaml.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using JustObjectsPrototype.Universal;
+using JustObjectsPrototype.Universal.JOP;
 using Windows.ApplicationModel.Activation;
+using Windows.Storage;
+using Windows.UI.Popups;
 using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
 
 namespace SurfaceXWing.CompanionApp
 {
@@ -17,28 +22,58 @@ namespace SurfaceXWing.CompanionApp
 			this.InitializeComponent();
 		}
 
-		protected override void OnLaunched(LaunchActivatedEventArgs e)
+		protected async override void OnLaunched(LaunchActivatedEventArgs e)
 		{
 			var objects = new ObservableCollection<object>
-			{
-				Squadron.Download("http://xwing-builder.co.uk/view/541496/first-try")
-			};
+			(
+				await Storage.AllSquadrons()
+			);
 
 			Show.Prototype(With.These(objects)
 				.AndViewOf<Squadron>()
-				.AndViewOf<Pilot>());
+				.AndViewOf<Pilot>()
+				.AndOpen<Squadron>());
 		}
 	}
 
-	[JustObjectsPrototype.Universal.JOP.Title("Squadrons"), JustObjectsPrototype.Universal.JOP.Icon(Windows.UI.Xaml.Controls.Symbol.AllApps)]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	[Title("Squadrons"), Icon(Symbol.AllApps)]
 	public class Squadron
 	{
 		private const string XWingBuilderBaseUrl = "http://xwing-builder.co.uk";
 
-		public string Name { get; private set; }
-		public string Url { get; private set; }
-
-		[JustObjectsPrototype.Universal.JOP.Editor(hide: true)]
+		[Editor(@readonly: true)]
+		public string Name { get; set; }
+		[Editor(@readonly: true)]
+		public string Url { get; set; }
+		[Editor(hide: true)]
 		public string Downloaded { get; set; }
 
 		public override string ToString()
@@ -46,36 +81,75 @@ namespace SurfaceXWing.CompanionApp
 			return Name;
 		}
 
-		[JustObjectsPrototype.Universal.JOP.Icon(Windows.UI.Xaml.Controls.Symbol.Download)]
-		public static Squadron Download(string url)
+		[Icon(Symbol.Download), JumpToResult]
+		public static async Task<Squadron> Download(string url = "http://xwing-builder.co.uk/view/541496/first-try")
 		{
-			var name = url.Split(new[] { "/" }, StringSplitOptions.RemoveEmptyEntries).Last();
-			var downloaded = AsyncHelpers.RunSync(() => Download(new Uri(url)));
+			if (!string.IsNullOrEmpty(url))
+			{
+				var name = url.Split(new[] { "/" }, StringSplitOptions.RemoveEmptyEntries).Last();
+				var downloaded = await Download(new Uri(url));
+				var newSquadron = new Squadron { Name = name, Url = url, Downloaded = downloaded };
 
-			return new Squadron { Name = name, Url = url, Downloaded = downloaded };
+				try
+				{
+					await Storage.Store(newSquadron);
+					return newSquadron;
+				}
+				catch (Exception e)
+				{
+					await new MessageDialog(e.Message, "Fehler").ShowAsync();
+					return null;
+				}
+			}
+			else
+			{
+				return null;
+			}
 		}
 
-		[JustObjectsPrototype.Universal.JOP.Icon(Windows.UI.Xaml.Controls.Symbol.Go)]
-		public void Aktivieren(ObservableCollection<Pilot> piloten)
+		async static Task<string> Download(Uri url)
+		{
+			var httpClient = new Windows.Web.Http.HttpClient();
+			return await httpClient.GetStringAsync(url);
+		}
+
+		[Icon(Symbol.Go), JumpToResult]
+		public async Task<List<Pilot>> Aktivieren(ObservableCollection<Pilot> piloten)
 		{
 			try
 			{
 				var parsedPilots = Parse();
+				if (!parsedPilots.Any())
+				{
+					throw new ArgumentException("Keine Piloten in diesem Squadron.");
+				}
 
 				piloten.Clear();
-				parsedPilots.ForEach(piloten.Add);
+
+				return parsedPilots;
 			}
 			catch (Exception e)
 			{
-				//todo: message box
+				await new MessageDialog(e.Message, "Fehler").ShowAsync();
+				return null;
 			}
 		}
 
-		[JustObjectsPrototype.Universal.JOP.Icon(Windows.UI.Xaml.Controls.Symbol.Delete)]
-		public void Löschen(ObservableCollection<Squadron> squadrons)
+		[Icon(Symbol.Delete)]
+		public async Task Löschen(ObservableCollection<Squadron> squadrons)
 		{
-			squadrons.Remove(this);
+			var löschDialog = new MessageDialog($"Suqadron \"{Name}\" löschen?");
+			löschDialog.Options = MessageDialogOptions.None;
+			löschDialog.Commands.Add(new UICommand("Ja", async cmd =>
+			{
+				await Storage.Remove(this);
+				squadrons.Remove(this);
+			}));
+			löschDialog.Commands.Add(new UICommand("Nein", cmd => { }));
+			löschDialog.CancelCommandIndex = 1;
+			löschDialog.DefaultCommandIndex = 0;
 
+			await löschDialog.ShowAsync();
 		}
 
 		private List<Pilot> Parse()
@@ -91,9 +165,9 @@ namespace SurfaceXWing.CompanionApp
 			var pilots = pilotMatches.Select(p => new Pilot
 			{
 				Name = p.titles.First().Groups[1].Value,
-				Image = XWingBuilderBaseUrl + p.images.First().Groups[1].Value,
+				Bild = XWingBuilderBaseUrl + p.images.First().Groups[1].Value,
 				Upgrades = p.titles.Skip(1).Zip(p.images.Skip(1), (t, i) => new { title = t, image = i })
-						.Select(u => new Upgrade { Image = XWingBuilderBaseUrl + u.image.Groups[1].Value, Name = u.title.Groups[1].Value })
+						.Select(u => new Upgrade { Bild = XWingBuilderBaseUrl + u.image.Groups[1].Value, Name = u.title.Groups[1].Value })
 						.ToList(),
 				Manoeuvers = new Manoeuvers
 				{
@@ -108,43 +182,129 @@ namespace SurfaceXWing.CompanionApp
 			}).ToList();
 			return pilots;
 		}
-
-		private async static Task<string> Download(Uri url)
-		{
-			var httpClient = new Windows.Web.Http.HttpClient();
-			return await httpClient.GetStringAsync(url);
-		}
 	}
 
-	[JustObjectsPrototype.Universal.JOP.Title("aktive Piloten"), JustObjectsPrototype.Universal.JOP.Icon(Windows.UI.Xaml.Controls.Symbol.Contact)]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	[Title("aktive Piloten"), Icon(Symbol.Contact)]
 	public class Pilot
 	{
-		[JustObjectsPrototype.Universal.JOP.Editor(hide: true)]
+		[Editor(hide: true)]
 		public string Name { get; set; }
 
-		[JustObjectsPrototype.Universal.JOP.CustomView("ImageDisplay")]
-		public string Image { get; set; }
+		[CustomView("ImageDisplay")]
+		public string Bild { get; set; }
 
-		[JustObjectsPrototype.Universal.JOP.CustomView("ManoeuversDisplay")]
+		public int Schilde { get; set; }
+		public int Hülle { get; set; }
+		public int Ausweichen { get; set; }
+		public int Fokus { get; set; }
+		public int Stress { get; set; }
+
+		[CustomView("ManoeuversDisplay")]
 		public Manoeuvers Manoeuvers { get; set; }
 
-		[JustObjectsPrototype.Universal.JOP.CustomView("ImagesDisplay")]
+		[CustomView("ImagesDisplay")]
 		public List<Upgrade> Upgrades { get; set; }
 
 		public override string ToString()
 		{
-			return "Pilot " + Name;
+			return Name;
 		}
 	}
 
 	public class Upgrade
 	{
-		public string Image { get; set; }
+		public string Bild { get; set; }
 		public string Name { get; set; }
 	}
 
 	public class Manoeuvers
 	{
 		public string[][] Grid { get; set; }
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	public static class Storage
+	{
+		public static async Task Store(Squadron squadron)
+		{
+			var file = await ApplicationData.Current.LocalFolder.CreateFileAsync(squadron.Name, CreationCollisionOption.FailIfExists);
+			using (var openedFile = await file.OpenAsync(FileAccessMode.ReadWrite))
+			using (var openedFileWriter = new StreamWriter(openedFile.AsStreamForWrite()))
+			{
+				await openedFileWriter.WriteLineAsync(squadron.Url);
+				await openedFileWriter.WriteAsync(squadron.Downloaded);
+				await openedFileWriter.FlushAsync();
+			}
+		}
+
+		public static async Task Remove(Squadron squadron)
+		{
+			var file = await ApplicationData.Current.LocalFolder.GetFileAsync(squadron.Name);
+			await file.DeleteAsync();
+		}
+
+		public static async Task<List<Squadron>> AllSquadrons()
+		{
+			var files = await ApplicationData.Current.LocalFolder.GetFilesAsync();
+			var squadrons = new List<Squadron>();
+			foreach (var file in files)
+			{
+				using (var openedFile = await file.OpenAsync(FileAccessMode.Read))
+				using (var openedFileReader = new StreamReader(openedFile.AsStreamForRead()))
+				{
+					var fileFirstLine = await openedFileReader.ReadLineAsync();
+					var fileContent = await openedFileReader.ReadToEndAsync();
+					squadrons.Add(new Squadron
+					{
+						Name = file.Name,
+						Url = fileFirstLine,
+						Downloaded = fileContent,
+					});
+				}
+			}
+			return squadrons;
+		}
 	}
 }
