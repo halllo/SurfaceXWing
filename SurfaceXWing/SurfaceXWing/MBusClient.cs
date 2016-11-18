ab7f664aeb1b71987f6ec674884e65d66900d7df
Manuel Naujoks <mnaujoks@gmail.com>
2016-11-18 21:02:43 +0100

    mbusclient

---
+++ SurfaceXWing/SurfaceXWing/MBusClient.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR.Client;
+
+namespace SurfaceXWing
+{
+	public class MBusClient : IDisposable
+	{
+		string _clientname;
+
+		HubConnection _connection;
+		IHubProxy _hubProxy;
+
+		public MBusClient(string clientname)
+		{
+			_clientname = clientname;
+		}
+
+		public Task Connect(string uri)
+		{
+			var connection = new HubConnection(uri);
+			connection.Closed += () =>
+			{
+				var eh = OnDisconnect;
+				if (eh != null) eh();
+			};
+			var hubProxy = connection.CreateHubProxy("MyHub");
+			hubProxy.On<string, string>("AddMessage", (userName, message) =>
+			{
+				var eh = On;
+				if (eh != null) eh(userName, message);
+			});
+
+			_connection = connection;
+			_hubProxy = hubProxy;
+
+			var task = connection.Start();
+			return task;
+		}
+
+		public string ConnectionId
+		{
+			get { return _connection.ConnectionId; }
+		}
+
+		public void Dispose()
+		{
+			_connection.Dispose();
+		}
+
+		public void Emit(string message)
+		{
+			_hubProxy.Invoke("Send", _clientname, message);
+		}
+
+		public event Action<string, string> On;
+		public event Action OnDisconnect;
+	}
+}
--- SurfaceXWing/SurfaceXWing/Spielfeld.xaml.cs
+++ SurfaceXWing/SurfaceXWing/Spielfeld.xaml.cs
@@ -5,16 +5,22 @@ namespace SurfaceXWing
 	public partial class Spielfeld
 	{
 		Game _Spiel;
+		RemoteGame _Remote;
 
 		public Spielfeld()
 		{
 			InitializeComponent();
-
-			Loaded += Setup;
-			SizeChanged += Setup;
+			
+			SizeChanged += (s, e) => Setup();
+			Loaded += (s, e) =>
+			{
+				Setup();
+				_Remote = new RemoteGame();
+				_Remote.ConnectToMBus();
+			};
 		}
 
-		private void Setup(object sender, System.Windows.RoutedEventArgs e)
+		private void Setup()
 		{
 			centerText.Text = "(" + ActualWidth + ", " + ActualHeight + ")";
 
@@ -32,5 +38,62 @@ namespace SurfaceXWing
 		}
 
 		public event Action<Game> GameStarted;
+	}
+
+
+
+
+
+
+
+
+
+
+	public class RemoteGame
+	{
+		MBusClient _MBus = new MBusClient("SurfaceXWing");
+
+		public void ConnectToMBus(string url = "http://mbusrelay.azurewebsites.net/signalr")
+		{
+			_MBus.OnDisconnect += Mbus_OnDisconnect;
+			_MBus.On += Mbus_On;
+			try
+			{
+				Log("connecting to " + url);
+				_MBus.Connect(url).Wait();
+				Log("connected");
+				_MBus.Emit("hallo");
+			}
+			catch (AggregateException e)
+			{
+				Log("cannot connect: " + e.InnerException.Message);
+			}
+		}
+
+		private void Log(string log)
+		{
+			System.Diagnostics.Debug.WriteLine(log);
+		}
+
+		private void Mbus_OnDisconnect()
+		{
+			Log("disconnect");
+		}
+
+		private void Mbus_On(string clientname, string message)
+		{
+			if (clientname.StartsWith("SurfaceXWing.CompanionApp"))
+			{
+				var messageItems = message.Split(new[] { ";" }, StringSplitOptions.None);
+				if (messageItems[0] == "refresh")
+				{
+					Log(clientname + ": " + message);
+				}
+				else if (messageItems[0] == "move")
+				{
+					Log(clientname + ": " + message);
+				}
+			}
+		}
 	}
 }
